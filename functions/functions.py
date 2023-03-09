6a2180f501ff03cbcc3ec6bd4c0ff26e41897b0c
dpalatynski <57257514+dpalatynski@users.noreply.github.com>
2023-03-09 22:03:54 +0100

    list of words added
    changes in creating json files

--- functions/functions.py
+++ functions/functions.py
@@ -1,18 +1,24 @@
 import json
+import os
 
 
 def create_dict_from_txt_file(txt_file: str) -> dict:
     """ Converts the txt file to a dictionary. One row in txt_file represents one flashcard, where at first there is
-    a target word and then divided by '--' a native word, e.g.: cat - kot. """
+    a name of word list, the second is target word and a native word, all divided by '--'
+    e.g.: animals -- cat -- kot. """
     with open(txt_file, 'r') as f:
         lines = f.readlines()
 
     words_dict = {}
     for line in lines:
         words = line.strip().split('--')
-        native_word = words[0].strip()
-        target_word = words[1].strip()
-        words_dict[native_word] = target_word
+        word_group = words[0].strip()
+        if word_group not in words_dict:  # create new list of words
+            words_dict[word_group] = {}
+
+        native_word = words[1].strip()
+        target_word = words[2].strip()
+        words_dict[word_group].update({native_word: target_word})
 
     return words_dict
 
@@ -28,3 +34,9 @@ def read_json_to_dict(words_json: str) -> dict:
     with open(words_json, 'r', encoding='utf-8') as f:
         words_dict = json.load(f)
     return words_dict
+
+
+if __name__ == '__main__':
+    # Create json from txt file
+    create_json_from_dict(create_dict_from_txt_file(os.path.join(os.pardir, 'data\\words.txt')),
+                          os.path.join(os.pardir, 'data\\flashcards.json'))
